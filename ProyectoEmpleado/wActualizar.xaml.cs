f638d04ba93d4219c20dca041c1780789bd35660
vladimir <ivladimirjuarez@outlook.com>
2017-10-25 13:05:38 -0500

    ventana actualizar

---
+++ ProyectoEmpleado/wActualizar.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProyectoEmpleado
+{
+    /// <summary>
+    /// Interaction logic for wActualizar.xaml
+    /// </summary>
+    public partial class wActualizar : Window
+    {
+        public wActualizar()
+        {
+            InitializeComponent();
+        }
+    }
+}
